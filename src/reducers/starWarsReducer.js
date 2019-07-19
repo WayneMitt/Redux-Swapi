95bd6c613dc678b3b9c658ee9cdfaba474e2219e
Wayne Mitt <wmitt85@gmail.com>
2019-07-18 19:40:49 -0500

    error characters is undefined

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,5 +1,6 @@
-import /* we need our action types here*/ "../actions";
+import { FETCH_DATA, SUCCESS, FAILURE } from /* we need our action types here*/ "../actions";
 const initialState = {
+  isLoading: false,
   characters: []
   // Array characters, Boolean fetching, null error.
 };
@@ -8,6 +9,27 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH_DATA: {
+      return {
+        ...state,
+        isLoading: true
+      }
+    }
+    case SUCCESS: {
+      const newData = action.payload
+      return {
+        ...state,
+        isLoading: false,
+        characters: newData
+      }
+    }
+    case FAILURE: {
+      return {
+        ...state,
+        isLoading: false,
+        errorMessage: action.payload.message
+      }
+    }
     default:
       return state;
   }
